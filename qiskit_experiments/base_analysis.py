9bf778441845fe4146eaba2028852e84729837f6
Christopher Wood <cjwood@us.ibm.com>
2021-05-04 17:50:21 -0400

    Rework option handling in base classes
    * Reworks how experiment, transpiler, backend/run, and analysis options are handled in the base classes to make it similar to how options are used by qiskit backends.
    
    * Removes transpiled_circuits method. Transpiled circuits can now be obtained via `transpile(expr.circuits(backend), backend, **expr.transpile_options)`

--- qiskit_experiments/base_analysis.py
+++ qiskit_experiments/base_analysis.py
@@ -14,7 +14,9 @@ Base analysis class.
 """
 
 from abc import ABC, abstractmethod
+import copy
 
+from qiskit.providers.options import Options
 from qiskit.exceptions import QiskitError
 from .experiment_data import ExperimentData, AnalysisResult
 
@@ -24,6 +26,36 @@ class BaseAnalysis(ABC):
 
     # Expected experiment data container for analysis
     __experiment_data__ = ExperimentData
+
+    def __init__(self, **options):
+        """Initialize a base analysis class
+
+        Args:
+            options: kwarg options for analysis.
+        """
+        self._options = self._default_options()
+        self.set_options(**options)
+
+    @classmethod
+    def _default_options(cls):
+        return Options()
+
+    def set_options(self, **fields):
+        """Set the analysis options.
+
+        Args:
+            fields: The fields to update the options
+        """
+        self._options.update_options(**fields)
+
+    @property
+    def options(self):
+        """Return the analysis options.
+
+        The options of an analysis class are used to provide kwarg values for
+        the :meth:`run` method.
+        """
+        return self._options
 
     def run(self, experiment_data, save=True, return_figures=False, **options):
         """Run analysis and update stored ExperimentData with analysis result.
@@ -35,7 +67,8 @@ class BaseAnalysis(ABC):
             return_figures (bool): if true return a pair of
                                    ``(analysis_results, figures)``,
                                     otherwise return only analysis_results.
-            options: kwarg options for analysis function.
+            options: additional analysis options. Any values set here will
+                     override the value from :meth:`options` for the current run.
 
         Returns:
             AnalysisResult: the output of the analysis that produces a
@@ -59,8 +92,14 @@ class BaseAnalysis(ABC):
         # Wait for experiment job to finish
         # experiment_data.block_for_result()
 
+        # Get runtime analysis options
+        analysis_options = copy.copy(self.options)
+        analysis_options.update_options(**options)
+        analysis_options = analysis_options.__dict__
+
         # Run analysis
-        analysis_results, figures = self._run_analysis(experiment_data, **options)
+        analysis_results, figures = self._run_analysis(experiment_data, **analysis_options)
+
         # Save to experiment data
         if save:
             if isinstance(analysis_results, AnalysisResult):
@@ -81,7 +120,9 @@ class BaseAnalysis(ABC):
 
         Args:
             experiment_data (ExperimentData): the experiment data to analyze.
-            options: kwarg options for analysis function.
+            options: additional options for analysis. By default the fields and
+                     values in :meth:`options` are used and any provided values
+                     can override these.
 
         Returns:
             tuple: A pair ``(analysis_results, figures)`` where
